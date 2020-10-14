b69f8c8e155931eafb343b30ea3e9631d170a414
JCoopzKnoxRox <62523192+JCoopzKnoxRox@users.noreply.github.com>
2020-10-14 14:13:22 -0500

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace asciiadventure
+{
+    class Player : MovingGameObject
+    {
+        public Player(int row, int col, Screen screen, string name) : base(row, col, "@", screen)
+        {
+            Name = name;
+        }
+        public string Name
+        {
+            get;
+            protected set;
+        }
+        public override Boolean IsPassable()
+        {
+            return true;
+        }
+
+        public String Action(int deltaRow, int deltaCol)
+        {
+            int newRow = Row + deltaRow;
+            int newCol = Col + deltaCol;
+            if (!Screen.IsInBounds(newRow, newCol))
+            {
+                return "nope";
+            }
+            GameObject other = Screen[newRow, newCol];
+            if (other == null)
+            {
+                return "negative";
+            }
+            // TODO: Interact with the object
+            if (other is Treasure)
+            {
+                other.Delete();
+                return "Yay, we got the treasure!";
+            }
+            return "ouch";
+        }
+        public bool Attack(GameObject go)
+        {
+                bool ret = false;
+                Random rand = new Random();
+                int ran = rand.Next(100);
+                if (ran < 75)
+                {
+                    ret = true;
+                }
+                else
+                {
+                    ret = false;
+                }
+            return ret;
+        }
+    }
+}
